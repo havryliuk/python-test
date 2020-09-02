27d639697f4b6828020343d3fed59c7209b71e4b
Oleksandr_Gavryliuk <oleksander.havryliuk@gmail.com>
2020-09-02 18:08:28 +0300

    Loops tutorial

---
+++ 2020-09-02_loops.py
@@ -0,0 +1,38 @@
+def while_loop():
+    secret = 'Swordfish'
+    guess = ''
+
+    while guess.lower() != secret.lower():
+        guess = input("What's the secret word? ")
+
+
+def for_loop():
+    animals = ('bear', 'bunny', 'dog', 'cat', 'cow')
+    for animal in animals:
+        print(animal, sep=' ')
+
+    for number in range(5, 10):
+        print(number)
+
+
+def additional_controls():
+    password = 'juswenko'
+    guess = ''
+    auth = False
+    attempt = 1
+    max_attempts = 5
+
+    while guess != password:
+        if attempt > max_attempts: break
+        guess = input(f"{attempt}: Enter the password: ")
+        attempt += 1
+    else:
+        auth = True
+
+    print("Authorised" if auth else "Permission denied")
+
+
+if __name__ == '__main__':
+    while_loop()
+    for_loop()
+    additional_controls()
